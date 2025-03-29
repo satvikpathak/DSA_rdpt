376edf92cc95f51ae8b383a1e687b7754ef2b2c4
satvikpathak007 <satvikpathak007@gmail.com>
2025-03-29 23:44:14 +0530

    pdone

--- pattern.cpp
+++ pattern.cpp
@@ -65,12 +65,19 @@ void pattern4_rev(int n) {
 
 int pattern5(int n){
     for(int i=0;i<2*n-1;i++){
-        
+        for(int j=0;j<2*n-1;j++){
+            int top=i;
+            int left=j;
+            int right=(2*n-2)-j;
+            int bottom=(2*n-2)-i;
+            cout<<n-min(min(top,left),min(right,bottom));
+        }
+        cout<<endl;
     }
 }
 
 int main(){
     int n;
     cin>>n;
-    pattern4(n);
+    pattern5(n);
 }
