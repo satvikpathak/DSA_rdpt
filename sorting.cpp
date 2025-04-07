21bc95b91a3cf8037a8cdf6021825af90399c015
satvikpathak007 <satvikpathak007@gmail.com>
2025-04-07 18:24:10 +0530

    sorting

--- sorting.cpp
+++ sorting.cpp
@@ -28,6 +28,18 @@ void bubble_sort(int arr[], int n) {
     }
 }
 
+void insertion_sort(int arr[], int n){
+    for(int i=0; i<=n-1; i++){
+        int j=i;
+        while(j>0 && arr[j] < arr[j-1]){
+            int temp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = temp;
+            j--;
+        }
+    }
+}
+
 int main() {
 
     //SELECTION SORT
@@ -44,6 +56,14 @@ int main() {
     // int arr[n];
     // for(int i=0;i<n;i++) cin>>arr[i];
     // bubble_sort(arr, n);
+    // for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+
+    //INSERTION SORT
+    // int n;
+    // cin>>n;
+    // int arr[n];
+    // for(int i=0;i<n;i++) cin>>arr[i];
+    // insertion_sort(arr, n);
     // for(int i=0;i<n;i++) cout<<arr[i]<<" ";
 
     return 0;
