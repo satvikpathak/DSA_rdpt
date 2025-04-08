be8ca704371c48c61e7857328b81b672788b867d
satvikpathak007 <satvikpathak007@gmail.com>
2025-04-08 22:28:38 +0530

    lc

---
+++ LC/LC_3396.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int minimumOperations(vector<int>& nums) {
+            unordered_set<int> seen;
+            for (int i = nums.size() - 1; i >= 0; --i) {
+                if (!seen.insert(nums[i]).second) {
+                    return (i + 1 + 2) / 3;
+                }
+            }
+            return 0;
+        }
+    };
+
+int main() {
+    Solution s;
+    vector<int> nums = {2,2,2,3,4,5,6,6,5,2,8,9,1};
+    cout << s.minimumOperations(nums) << endl;
+    return 0;
+}
