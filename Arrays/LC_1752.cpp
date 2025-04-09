b70c68b8d398345bbd9e8029b088dca97ced0825
satvikpathak007 <satvikpathak007@gmail.com>
2025-04-09 22:45:03 +0530

    arrays

---
+++ Arrays/LC_1752.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        bool check(vector<int>& nums) {
+            int count = 0;
+            int n = nums.size();
+            
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] > nums[(i + 1) % n]) {
+                    ++count;
+                }
+                if (count > 1) {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    };
+
+int main() {
+    Solution s;
+    vector<int> nums = {3, 4, 5, 1, 2};
+    cout << s.check(nums) << endl;
+    return 0;
+}
