bfb10845788f230265fd0c06f17a499cfe36dd9e
satvikpathak007 <satvikpathak007@gmail.com>
2025-04-11 21:36:31 +0530

    practice palin

--- practice.cpp
+++ practice.cpp
@@ -37,6 +37,20 @@ void insertion_sort(int arr[], int n){
     }
 }
 
+bool isPalindrome(int x) {
+    int palindrome = 0;
+    int y=x;
+
+    if(x < 0) return false;
+
+    while(x != 0) {
+        int last_digit = x % 10;
+        palindrome = palindrome * 10 + last_digit;
+        x /= 10;
+    }
+    return (palindrome == y);
+}
+
 int main() {
     int n;
     cin>>n;
