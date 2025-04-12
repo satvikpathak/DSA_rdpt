23b82f74f5eed6ade0b06093928d7615eae63b49
satvikpathak007 <satvikpathak007@gmail.com>
2025-04-12 22:24:12 +0530

    wifi

--- practice.cpp
+++ practice.cpp
@@ -75,6 +75,11 @@ int fib(int n) {
     return fib(n-1) + fib(n-2);
 }
 
+int fib(int n) {
+    if(n==0 || n==1) return n;
+    return fib(n-1) + fib(n-2);
+}
+
 int main() {
     int n;
     cin>>n;
