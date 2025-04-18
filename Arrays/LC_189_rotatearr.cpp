4972a0495f01819c5683fc8202528a6ee2fc6fd6
satvikpathak007 <satvikpathak007@gmail.com>
2025-04-19 00:08:44 +0530

    array

---
+++ Arrays/LC_189_rotatearr.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        void rotate(vector<int>& nums, int k) {
+            int n = nums.size();
+            k = k % n;
+    
+            reverse(nums.begin(), nums.end());
+    
+            reverse(nums.begin(), nums.begin() + k);
+    
+            reverse(nums.begin() + k, nums.end());
+        }
+    };
+    
+
+int main() {
+    Solution s;
+    vector<int> nums = {1,2,3,4,5,6,7};
+    int k = 3;
+    s.rotate(nums, k);
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
