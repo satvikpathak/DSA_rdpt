2bb78ebd595fe642abf9be59b73da45101e3ce24
satvikpathak007 <satvikpathak007@gmail.com>
2025-04-26 22:58:32 +0530

    one

---
+++ Arrays/LC_485_maxconsecones.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int findMaxConsecutiveOnes(vector<int>& nums) {
+            int maxi = 0;
+            int cnt = 0;
+            for(int i=0; i<nums.size(); i++){
+                if(nums[i] == 1){
+                    cnt ++;
+                    maxi = max(maxi, cnt);
+                }
+                else{
+                    cnt = 0;
+                }
+            }
+    
+            return maxi;
+        }
+    };
+
+int main() {
+    Solution s;
+    vector<int> nums = {1, 1, 0, 1, 1, 1};
+    cout << s.findMaxConsecutiveOnes(nums) << endl;
+    return 0;
+}
