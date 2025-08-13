2532789a56e1a147de60704262dcd35a50a9631c
satvikpathak <satvikpathak007@gmail.com>
2025-08-13 23:51:44 +0530

    back

--- Arrays/demo.cpp
+++ Arrays/demo.cpp
@@ -1,1 +1,20 @@
-//0,1,2 brute approach sorting preferred merge sort then hash map then optimal
+#include<bits/stdc++.h>
+using namespace std;
+
+int largest(int arr[],int n){
+    int largest = arr[0];
+    for(int i=0; i<n; i++){
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+    return largest;
+
+}
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+    largest(arr, 5);
+    cout<< "Largest element is: " << largest(arr, 5) << endl;
+    return 0;
+}
