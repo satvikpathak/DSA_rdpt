6ee789df3cbf0bf2685cf3d16146eafcfc19d4d4
satvikpathak <satvikpathak007@gmail.com>
2026-01-03 23:25:54 +0530

    lc_509

--- LC/LC_509.cpp
+++ LC/LC_509.cpp
@@ -1,18 +1,28 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+//     public:
+//         int fib(int n) {
+//             if(n==0 || n==1) return n;
+//             return fib(n-1) + fib(n-2);
+//         }
+//     };
+
+// int main() {
+//     Solution obj;
+//     int n;
+//     cin>>n;
+//     cout<<obj.fib(n);
+//     return 0;
+// }
 
 class Solution {
-    public:
-        int fib(int n) {
-            if(n==0 || n==1) return n;
-            return fib(n-1) + fib(n-2);
-        }
-    };
+public:
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
 
-int main() {
-    Solution obj;
-    int n;
-    cin>>n;
-    cout<<obj.fib(n);
-    return 0;
-}
+        else return fib(n-1) + fib(n-2);
+    }
+};
