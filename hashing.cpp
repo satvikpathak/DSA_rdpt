81545320b3756bacebc343ed1296a766e1e1e9eb
satvikpathak <satvikpathak007@gmail.com>
2026-01-04 13:25:21 +0530

    hashing basics

--- hashing.cpp
+++ hashing.cpp
@@ -1,7 +1,7 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int main() {
+// int main() {
     // int n;
     // cin>>n;
     // int arr[n];
@@ -24,30 +24,56 @@ int main() {
     //     cout<<hash[number]<<endl;
     // }
 
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     //precompute
+//     map<int, int> mpp; // unordered_map if order doesn't matter
+//     for(int i=0;i<n;i++){
+//         mpp[arr[i]] ++;
+//     }
+
+//     for (auto it : mpp){
+//         cout<<it.first<<" "<<it.second<<endl;
+//     }
+
+
+//     int q;
+//     cin>>q;
+//     while(q--){
+//         int number;
+//         cin>>number;
+//         // fetch
+//         cout<<mpp[number]<<endl;
+//     }
+//     return 0;
+// } 
+
+//character hashing
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+
     //precompute
-    map<int, int> mpp; // unordered_map if order doesn't matter
-    for(int i=0;i<n;i++){
-        mpp[arr[i]] ++;
+    int hash[26] = {0};
+    for(int i=0; i<s.size(); i++){
+        hash[s[i] - 'a'] ++;
     }
-
-    for (auto it : mpp){
-        cout<<it.first<<" "<<it.second<<endl;
-    }
-
 
     int q;
     cin>>q;
     while(q--){
-        int number;
-        cin>>number;
-        // fetch
-        cout<<mpp[number]<<endl;
+        char c;
+        cin>>c;
+        //fetch
+        cout<<hash[c -'a']<<endl;
     }
+
     return 0;
-} 
+}
