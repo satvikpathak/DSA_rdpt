2a83f7954b7980a0eb0fca542cc110604932d110
satvikpathak <satvikpathak007@gmail.com>
2026-01-05 23:15:02 +0530

    prep

--- demo.cpp
+++ demo.cpp
@@ -188,3 +188,31 @@
 
 //     return 0;
 // }
+
+
+//practice LC 977: Squares of a Sorted Array
+// optimized 2 pointer approach
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> numsq(n);
+
+//         int left = 0;
+//         int right = n-1;
+
+//         for(int i=n-1; i>=0; i--){
+//             if(abs(nums[left]) > abs(nums[right])){
+//                 numsq[i] = nums[left] * nums[left];
+//                 left++;
+//             }
+
+//             else{
+//                 numsq[i] = nums[right] * nums[right];
+//                 right--;
+//             }
+//         }
+
+//         return numsq;
+//     }
+// };
