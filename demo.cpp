ba00df4d5df701feabe4af54cd28e71475151ab8
satvikpathak <satvikpathak007@gmail.com>
2026-01-05 23:16:05 +0530

    prep

--- demo.cpp
+++ demo.cpp
@@ -216,3 +216,22 @@
 //         return numsq;
 //     }
 // };
+
+
+//practice LC 344: Reverse String
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//         int left = 0;
+//         int right = s.size() - 1;
+
+//         while(left<right){
+//             char temp = s[left];
+//             s[left] = s[right];
+//             s[right] = temp;
+
+//             left ++;
+//             right --;
+//         }
+//     }
+// };
