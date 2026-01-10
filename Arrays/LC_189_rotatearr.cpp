b5e57d4b6f2f95c334e1bd52ddf071e3f9eab8ea
satvikpathak <satvikpathak007@gmail.com>
2026-01-11 00:00:11 +0530

    lc_189

--- Arrays/LC_189_rotatearr.cpp
+++ Arrays/LC_189_rotatearr.cpp
@@ -47,3 +47,17 @@ int main(){
 //             reverse(nums.begin() + k, nums.end());
 //         }
 //     };
+
+class Solution {
+    public:
+        void rotate(vector<int>& nums, int k) {
+            int n = nums.size();
+            k = k % n;
+    
+            reverse(nums.begin(), nums.end());
+    
+            reverse(nums.begin(), nums.begin() + k);
+    
+            reverse(nums.begin() + k, nums.end());
+        }
+    };
