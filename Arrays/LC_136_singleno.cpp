3b2fe2e48f5573f72e9db2c6715b7fae9c7580f1
satvikpathak <satvikpathak007@gmail.com>
2026-01-12 23:30:43 +0530

    lc_136

--- Arrays/LC_136_singleno.cpp
+++ Arrays/LC_136_singleno.cpp
@@ -19,3 +19,15 @@ int main() {
     cout << s.singleNumber(nums) << endl;
     return 0;
 }
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++){
+            ans = ans^nums[i];
+        }
+
+        return ans;
+    }
+};
