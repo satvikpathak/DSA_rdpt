d7906893b50137c88a01dce879f1a2e02ad6a7f2
satvikpathak <satvikpathak007@gmail.com>
2026-01-13 23:37:55 +0530

    lc_1 two sum

--- Arrays/LC_1_two_sum.cpp
+++ Arrays/LC_1_two_sum.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+//better approach using map(if i have to return indices, this is best but if only yes/no then use two pointer since no storing indices needed)
 class Solution {
     public:
         vector<int> twoSum(vector<int>& nums, int target) {
@@ -25,3 +27,22 @@ int main() {
     for(int i=0; i<ans.size(); i++) cout<<ans[i]<<" ";
     return 0;
 }
+
+//brute force approach
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i]+nums[j]==target){
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+};
