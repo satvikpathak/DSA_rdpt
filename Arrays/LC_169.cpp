f43aa4d550a83815e5cc9a34f102bab388fa750c
satvikpathak <satvikpathak007@gmail.com>
2026-01-20 22:49:07 +0530

    return

---
+++ Arrays/LC_169.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (cnt == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+
+        return candidate;
+    }
+};
