c698ba11f820764603555bc0cfb7ff0036ed48d0
satvikpathak <satvikpathak007@gmail.com>
2026-03-25 22:32:12 +0530

    lc_2149

--- Arrays/LC_2149. Rearrange Array Elements by Sign.cpp
+++ Arrays/LC_2149. Rearrange Array Elements by Sign.cpp
@@ -1,9 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        int posIndex = 0; int negIndex = 1;
 
+        for(int i=0; i<n; i++){
+            if(nums[i] < 0){
+                ans[negIndex] = nums[i];
+                negIndex +=2;
+            }
+
+            else{
+                ans[posIndex] = nums[i];
+                posIndex +=2;
+            }
+        }
+
+        return ans;
+    }
+};
 
 int main(){
-    
+    vector<int> nums{-1,1,-2,2,-3,3,-4,4};
+    Solution sol;
+    vector<int> ans = sol.rearrangeArray(nums);
+    for(int i=0; i<ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
     return 0;
 }
