cf78e690243d64de94dca9ed5b8a9f4365be7d17
satvikpathak <satvikpathak007@gmail.com>
2026-03-25 23:09:40 +0530

    lc_31

---
+++ Arrays/LC_31_next_permutation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//STL version, may ask you to implement it without STL
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
+
+//implementation without STL
+
+
+int main(){
+    
+    return 0;
+}
