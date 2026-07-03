c944b7bbb96b9d8e44dd3f8934520e66258e117c
satvikpathak <satvikpathak007@gmail.com>
2026-07-03 23:47:38 +0530

    LC_1

--- Arrays/LC_1_two_sum.cpp
+++ Arrays/LC_1_two_sum.cpp
@@ -50,7 +50,7 @@ public:
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> mpp;
+        unordered_map<int, int> mpp;
         for(int i=0; i<nums.size(); i++){
             int more = target - nums[i];
             if(mpp.find(more) != mpp.end()){
