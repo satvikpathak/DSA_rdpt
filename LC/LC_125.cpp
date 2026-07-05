b42637748247e4dc1b63f5492799fbff76d62c3f
satvikpathak <satvikpathak007@gmail.com>
2026-07-05 17:25:02 +0530

    lc_977 square of sorted array

--- LC/LC_125.cpp
+++ LC/LC_125.cpp
@@ -6,7 +6,7 @@ class Solution {
         bool isPalindrome(string s) {
            int start=0;
            int end=s.size()-1;
-           while(start<=end){
+           while(start<end){
                if(!isalnum(s[start])){start++; continue;}
                if(!isalnum(s[end])){end--;continue;}
                if(tolower(s[start])!=tolower(s[end]))return false;
