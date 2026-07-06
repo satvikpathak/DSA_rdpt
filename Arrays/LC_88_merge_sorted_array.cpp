3dead66d9b315fd14e78d243afced7c73df520d4
satvikpathak <satvikpathak007@gmail.com>
2026-07-06 23:46:31 +0530

    lc_88 optimal three pointer

--- Arrays/LC_88_merge_sorted_array.cpp
+++ Arrays/LC_88_merge_sorted_array.cpp
@@ -1,13 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//brute
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         for (int i = 0; i < n; i++) {
+//             nums1[m + i] = nums2[i];
+//         }
+//         sort(nums1.begin(), nums1.end());
+//     }
+// };
+
+//optimal
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = 0; i < n; i++) {
-            nums1[m + i] = nums2[i];
+        int right1 = m - 1;
+        int right2 = n - 1;
+        int tPtr = m + n - 1;
+
+        while(right1 >= 0 && right2 >= 0){
+            if(nums1[right1] > nums2[right2]){
+                nums1[tPtr] = nums1[right1]; 
+                right1--;
+                tPtr--;
+            }
+
+            else{
+                nums1[tPtr] = nums2[right2];
+                right2--;
+                tPtr--;
+            }
         }
-        sort(nums1.begin(), nums1.end());
+
+        while(right2 >= 0){
+            nums1[tPtr] = nums2[right2];
+            right2--;
+            tPtr--;
+        }
     }
 };
 
