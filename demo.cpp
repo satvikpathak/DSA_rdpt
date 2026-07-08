31b980ee3eba8e88c4d6e4a51821a1d7b83823a0
satvikpathak <satvikpathak007@gmail.com>
2026-07-08 19:03:00 +0530

    rev 283

--- demo.cpp
+++ demo.cpp
@@ -343,3 +343,24 @@ int main(){
 //     }
 //     }
 // };
+
+//lc283
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int left = 0;
+//         int right = 0;
+
+//         for(int right =0; right< nums.size(); right ++){
+//             if(nums[right] != 0){
+//                 nums[left] = nums[right];
+//                 left++;
+//             }
+//         }
+
+//         while(left < nums.size()){
+//             nums[left] = 0;
+//             left ++;
+//         }
+//     }
+// };
