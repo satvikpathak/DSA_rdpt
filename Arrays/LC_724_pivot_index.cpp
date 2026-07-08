cd279494eb25e39312bd9c9dd39efbf6047a7ee1
satvikpathak <satvikpathak007@gmail.com>
2026-07-08 18:01:41 +0530

    LC_724 pivot index

---
+++ Arrays/LC_724_pivot_index.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0;
+        int leftSum = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if(leftSum == sum - leftSum - nums[i]){
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
+
+int main(){
+    
+    
+    return 0;
+}
