ee3c85b7f2780941fb8252484d49aaef07452bb9
satvikpathak <satvikpathak007@gmail.com>
2026-07-10 22:25:39 +0530

    revision lc_876,206,146 LL

--- demo.cpp
+++ demo.cpp
@@ -507,3 +507,27 @@ int main(){
 //         return slow;
 //     }
 // };
+
+//209
+// class Solution {
+// public:
+//     int minSubArrayLen(int target, vector<int>& nums) {
+//         int left = 0;
+//         int right = 0;
+//         int sum = 0;
+//         int length = INT_MAX;
+
+//         while(right < nums.size()){
+//             sum += nums[right];
+//             while(sum >= target){
+//                 length = min(length, right - left + 1);
+//                 sum -= nums[left];
+//                 left++;
+//             }
+//             right ++;
+//         }
+
+//         if(length == INT_MAX) return 0;
+//         else return length;
+//     }
+// };
