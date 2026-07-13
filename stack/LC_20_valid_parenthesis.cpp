23097cc1600b3086181fb5ed6b62080478555c62
satvikpathak <satvikpathak007@gmail.com>
2026-07-13 22:59:18 +0530

    lc_20 valid paren

---
+++ stack/LC_20_valid_parenthesis.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c : s){
+            if(c == '('){
+                st.push(')');
+            }
+            else if(c == '{'){
+                st.push('}');
+            }
+            else if(c == '['){
+                st.push(']');
+            }
+            else{
+                if(st.empty() || st.top() != c){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+int main(){
+    
+    return 0;
+}
