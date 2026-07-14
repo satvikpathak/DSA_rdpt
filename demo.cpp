99199f42ee93b04a6e855769f88fec5e9b6b4242
satvikpathak <satvikpathak007@gmail.com>
2026-07-14 22:21:49 +0530

    rev

--- demo.cpp
+++ demo.cpp
@@ -708,3 +708,60 @@ int main(){
 //         return res;
 //     }
 // };
+
+// class Solution {
+// public:
+//     int left(vector<int>& nums, int target){
+//         int left = 0;
+//         int right = nums.size() - 1;
+//         int ans = -1;
+
+//         while(left <= right){
+//             int mid = left + (right - left)/2;
+
+//             if(nums[mid] == target){
+//                 ans = mid;
+//                 right = mid - 1;
+//             }
+
+//             else if(nums[mid] > target){
+//                 right = mid - 1;
+//             }
+
+//             else{
+//                 left = mid + 1;
+//             }
+//         }
+
+//         return ans;
+//     }
+
+//     int right(vector<int>& nums, int target){
+//         int left = 0;
+//         int right = nums.size() - 1;
+//         int ans = -1;
+
+//         while(left <= right){
+//             int mid = left + (right - left)/2;
+
+//             if(nums[mid] == target){
+//                 ans = mid;
+//                 left = mid + 1;
+//             }
+
+//             else if(nums[mid] < target){
+//                 left = mid + 1;
+//             }
+
+//             else{
+//                 right = mid - 1;
+//             }
+//         }
+
+//         return ans;
+//     }
+
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         return {left(nums, target), right(nums, target)};
+//     }
+// };
